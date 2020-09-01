ab8b2c6ed51824605cff2b6abca7f9c9151f9592
Evgeniya Brezhe <for.zhenya.popova@gmail.com>
2020-09-01 09:23:25 +0300

    Commit Message

--- HW/123.py
+++ HW/123.py
@@ -1,5 +1,6 @@
 import random
 import string
+import re
 #
 # # def id_generator():
 # #     return ''.join(random.choice(string.ascii_letters + string.digits + " " + string.punctuation) for value in range(random.randint(100, 1000)))
@@ -46,18 +47,33 @@ import string
 #     return my_new_str
 
 
-persons = [{'name': 'John1 Alfa', 'age': 20},
-           {'name': 'Alma Beta', 'age': 21},
-           {'name': 'Hendrick Gamma', 'age': 19}]
+# persons = [{'name': 'Joh1231asdan124 2342 2342 234234 23423 3434 34343  Alfa', 'age': 20},
+#            {'name': 'Alm1231a Beta', 'age': 21},
+#            {'name': 'Hen1231drick Gamma', 'age': 19}]
+#
+# str_1 = '4wrtb34qvj3hbq3itq35t'
+# str_2 = persons[0]
+# str_3 = str_2['name']
+# print(str_3, type(str_3))
+# date = re.findall(r'[0-9]{3,4}', str_3)
+# print(date)
+# some_str = '1231 22 2233 f sdfsd sdg BC 1212 222 '
+# date = re.findall(r'[0-9]{3,4}', some_str)
+# print(date)
+# if some_str.find('BC') == -1:
+#     print('no')
+# else:
+#     print('yes')
+
 
 # for index in range(len(persons)):
 #     my_dict_value = persons[index]
 #     my_value = my_dict_value.get('name')
-
-    print(my_value)
-
-
-print(len(persons))
+#
+#     print(my_value)
+#
+#
+# print(len(persons))
 # new_persons = sorted(persons, key=sort_key)
 # print(new_persons)
 
@@ -65,3 +81,74 @@ print(len(persons))
 # b = a.split(" ")
 # my_list = b[len(b) - 1]
 # print(my_list)
+
+# my_list = [
+#     {"name": "Brahmagupta",
+#      "years": "597 – 668."},
+#     {"name": "Rene Descartes",
+#      "years": "1596 – 1650."},
+#     {"name": "Euclid",
+#      "years": "c. 325 – c. 270 BC."}]
+#
+# year_1 = my_list[0]
+# year_2 = my_list[1]
+# year_3 = my_list[2]
+# print(year_1, year_2, year_3, type(year_3))
+# print('year_1')
+#
+# year_from_dict_1 = year_1['years'].split('-')
+# year_from_dict_2 = year_2['years'].split('-')
+# year_from_dict_3 = year_3['years'].split('-')
+# print(year_from_dict_1, year_from_dict_2, year_from_dict_3, type(year_from_dict_3))
+# print('year_from_dict_1')
+#
+# str_years_1 = str(year_from_dict_1)
+# str_years_2 = str(year_from_dict_2)
+# str_years_3 = str(year_from_dict_3)
+# print(str_years_1, str_years_2, str_years_3, type(str_years_3))
+# print('str_years_1')
+#
+# place_1 = str_years_1.find('–')
+# place_2 = str_years_2.find('–')
+# place_3 = str_years_3.find('–')
+# print(place_1, place_2, place_3)
+#
+# str_death_1 = str_years_1[place_1:len(str_years_1)]
+# str_death_2 = str_years_2[place_2:len(str_years_2)]
+# str_death_3 = str_years_3[place_3:len(str_years_3)]
+# print(str_death_1, str_death_2, str_death_3)
+#
+# date_1 = int(re.findall(r'\d+', str_years_1)[1])
+# date_2 = int(re.findall(r'\d+', str_years_2)[1])
+# date_3 = int(re.findall(r'\d+', str_years_3)[1])
+# print(date_1, date_2, date_3, type(date_3))
+#
+# print('-----------')
+# if 'BC' not in str_death_1:
+#     end_date_1 = date_1
+# else:
+#     end_date_1 = date_1 * - 1
+#
+# print(end_date_1, type(end_date_1))
+#
+# if 'BC' not in str_death_2:
+#     end_date_2 = date_2
+# else:
+#     end_date_2 = date_2 * - 1
+#
+# print(end_date_2)
+#
+# if 'BC' not in str_death_3:
+#     end_date_3 = date_3
+# else:
+#     end_date_3 = date_3 * - 1
+#
+# print(end_date_3)
+
+line = "Fibonacci’s Book"
+count = len(re.findall(r'[a-zA-z’]+', line))
+print(count)
+
+
+
+
--- HW/Brezhe_E_lesson11_hw.py
+++ HW/Brezhe_E_lesson11_hw.py
@@ -1,5 +1,6 @@
 # data.json - файл с данными о некоторых математиках прошлого.
 import json
+import re
 
 
 # 1. Необходимо написать функцию, которая считает эти данные из файла. Параметр функции - имя файла.
@@ -14,15 +15,15 @@ def json_reader(filename):
 # Например для Rene Descartes фамилия это Descartes, у Pierre de Fermat - Fermat и т.д.
 # Если фамилии нет, то использовать имя, например Euclid.
 
-def sort_key(dict_value):
+def sort_key_last_name(dict_value):
     name_from_dict = dict_value["name"]
     split_list_of_values = name_from_dict.split(" ")
     last_name = split_list_of_values[len(split_list_of_values) - 1]
     return last_name
 
 
-def sort_function():
-    sorted_dict = sorted(json_reader('data.json'), key=sort_key)
+def sort_function_2():
+    sorted_dict = sorted(json_reader('data.json'), key=sort_key_last_name)
     print(sorted_dict)
 
 
@@ -30,5 +31,31 @@ def sort_function():
 # Обратите внимание на сокращение BC. - это означает до н.э.
 
 
+def sort_key_date(dict_value):
+    years_from_dict = dict_value['years']
+    dash_place = years_from_dict.find('–')
+    str_death = years_from_dict[dash_place:len(years_from_dict)]
+    almost_death_date = int(re.findall(r'\d+', years_from_dict)[1])
+    if 'BC' not in str_death:
+        death_date = almost_death_date
+    else:
+        death_date = almost_death_date * - 1
+    return death_date
+
+
+def sort_function_3():
+    sorted_dict = sorted(json_reader('data.json'), key=sort_key_date)
+    print(sorted_dict)
+
 
 # 4. Написать функцию сортировки по количеству слов в поле "text"
+
+def sort_key_count(dict_value):
+    text_from_dict = dict_value['text']
+    word_count = len(re.findall(r'[a-zA-z’]+', text_from_dict))
+    return word_count
+
+
+def sort_function_4():
+    sorted_dict = sorted(json_reader('data.json'), key=sort_key_count)
+    print(sorted_dict)
